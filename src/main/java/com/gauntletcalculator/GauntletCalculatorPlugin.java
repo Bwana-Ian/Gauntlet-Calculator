0ff64598f4df254d0dffcdab23ecdac22d131da4
Bwana-Ian <iansiebold+dev@gmail.com>
2021-12-28 12:44:43 -0600

    Added the plugin

--- src/main/java/com/gauntletcalculator/GauntletCalculatorPlugin.java
+++ src/main/java/com/gauntletcalculator/GauntletCalculatorPlugin.java
@@ -11,43 +11,47 @@ import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.ui.ClientToolbar;
+import net.runelite.client.ui.NavigationButton;
+import net.runelite.client.util.ImageUtil;
+
+import java.awt.image.BufferedImage;
 
 @Slf4j
 @PluginDescriptor(
-	name = "gauntlet-calculator"
+	name = "Gauntlet Calculator",
+	description = "Calculates how many crystal shards and resources you need in the Gauntlet",
+	tags = {"Corrupted", "Gauntlet", "Bwana Ian", "Resources", "Crystal Shards"}
 )
 public class GauntletCalculatorPlugin extends Plugin
 {
 	@Inject
-	private Client client;
+	private ClientToolbar clientToolbar;
 
-	@Inject
-	private GauntletCalculatorConfig config;
-
-	@Override
-	protected void startUp() throws Exception
-	{
-		log.info("gauntlet-calculator started!");
-	}
+	private GauntletCalculatorPanel panel;
+	private NavigationButton navButton;
 
 	@Override
-	protected void shutDown() throws Exception
-	{
-		log.info("gauntlet-calculator stopped!");
+	protected void startUp(){
+		startPanel();
 	}
 
-	@Subscribe
-	public void onGameStateChanged(GameStateChanged gameStateChanged)
-	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
-		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "gauntlet-calculator says " + config.greeting(), null);
-		}
+	@Override
+	protected void shutDown(){
+		clientToolbar.removeNavigation(navButton);
 	}
 
-	@Provides
-	GauntletCalculatorConfig provideConfig(ConfigManager configManager)
-	{
-		return configManager.getConfig(GauntletCalculatorConfig.class);
+	private void startPanel(){
+		final BufferedImage icon = ImageUtil.loadImageResource(getClass(), "Crystal_shard.png");
+		panel = injector.getInstance(GauntletCalculatorPanel.class);
+
+		navButton = NavigationButton.builder()
+				.tooltip("Gauntlet Calculator")
+				.icon(icon)
+				.priority(6)
+				.panel(panel)
+				.build();
+
+		clientToolbar.addNavigation(navButton);
 	}
 }
